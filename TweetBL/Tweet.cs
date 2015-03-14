9e106503b0a3b0a2b310695a043383bd7c20ad74
Rahul-P <rahulpawar@y7mail.com>
2015-03-14 18:53:09 +1100

    Further modifications to Web Service Project.

--- TweetBL/Tweet.cs
+++ TweetBL/Tweet.cs
@@ -7,12 +7,16 @@ namespace TweetBL
     // This is our Data Object
 
     [Serializable]
+    [DataContract]
     public class Tweet
     {
+        [DataMember]
         public int Id { get; set; }
-  
+
+        [DataMember]
         public string PostedBy { get; set; }
-       
+
+        [DataMember]
         public string Text { get; set; }
     }
 }
